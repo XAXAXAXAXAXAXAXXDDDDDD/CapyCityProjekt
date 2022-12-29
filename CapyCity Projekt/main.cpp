ff3d2caa0aac4e190e1b18b23db875b1573ec7e5
XAXAXAXAXAXAXAXXDDDDDD <kaikong03@gmail.com>
2022-12-29 12:13:54 +0100

    Update

--- CapyCity Projekt/main.cpp
+++ CapyCity Projekt/main.cpp
@@ -15,14 +15,23 @@ int Height;
 int Width;
 Building** plan;
 
+/// <summary>
+/// 
+/// </summary>
 static std::unordered_map<std::string, Building> const table = { {"Empty", Empty}, {"House", House}, {"Farm", Farm} };
 
+/// <summary>
+/// Shows the menu for users to choose action.
+/// </summary>
+/// <returns></returns>
 Action ShowMenu() {
 	string menu = "Menue: \nDruecke 's' um Gebauede zu setzen\nDruecke 'l' um Bereich zu loeschen\nDruecke 'a' um Plan auszugeben\nDruecke 'e' um Programm zu beenden";
 	cout << menu << endl;
+	// read user input
 	string input;
 	cin >> input;
 
+	// return corresponding action
 	if (input._Equal("s")) {
 		return Place;
 	}
@@ -40,12 +49,20 @@ Action ShowMenu() {
 	}
 }
 
-
+/// <summary>
+/// Helper method for finding overlapping buildings in the given square.
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+/// <returns></returns>
 bool FindOverlappingBuildings(int x, int y, int width, int height) {
+
+	// iterate through part of plan array 
 	for (int i = x; i < x + width; i++) {
 		for (int j = y; j < y + height; j++) {
 			if (plan[i][j] != Empty) {
-				cout << "Kann kein Gebäude setzen! Es existiert bereits eins an dieser Stelle!" << endl;
 				return true;
 			}
 		}
@@ -53,6 +70,10 @@ bool FindOverlappingBuildings(int x, int y, int width, int height) {
 	return false;
 }
 
+/// <summary>
+/// Action Place Building.
+/// </summary>
+/// <returns>Bool determining whether placement was successful.</returns>
 bool PlaceBuilding() {
 	int heightOfBuilding;
 	int widthOfBuilding;
@@ -63,29 +84,41 @@ bool PlaceBuilding() {
 
 	printf("Art (%s, %s): ", buildings[House].c_str(), buildings[Farm].c_str());
 	cin >> typeString;
+	// find user input
 	auto it = table.find(typeString);
 	if (it != table.end()) {
 		type = it->second;
 	}
 	else {
+		cout << "Dieser Gebaeudetyp existiert nicht!" << endl;
 		return false;
 	}
 
-	cout << "Hoehe: " << endl;
-	cin >> heightOfBuilding;
-	if (heightOfBuilding < 0 || heightOfBuilding > Height) {
-		return false;
-	}
+	// read and check width input
 	cout << "Breite: " << endl;
 	cin >> widthOfBuilding;
 	if (widthOfBuilding < 0 || widthOfBuilding > Width) {
+		cout << "Ups! Breite ist zu klein oder zu groß." << endl;
 		return false;
 	}
+
+	// read and check height input
+	cout << "Hoehe: " << endl;
+	cin >> heightOfBuilding;
+	if (heightOfBuilding < 0 || heightOfBuilding > Height) {
+		cout << "Ups! Hoehe ist zu klein oder zu groß." << endl;
+		return false;
+	}
+
+	// read and check x coordinate input
 	cout << "x-Koordinate: " << endl;
 	cin >> xPosition;
 	if (xPosition < 0 || xPosition + widthOfBuilding > Width) {
+		cout << "Ups! Das Gebaeude ragt ueber die verfuegbare Flaeche hinaus." << endl;
 		return false;
 	}
+
+	// read and check y coordinate input
 	cout << "y-Koordinate: " << endl;
 	cin >> yPosition;
 	// test of size of:
@@ -94,42 +127,63 @@ bool PlaceBuilding() {
 	//cout << (sizeof(plan) / sizeof(plan[0])) << endl;
 	//cout << (sizeof(plan[0]) / sizeof(plan[0][0])) << endl;
 	if (yPosition < 0 || yPosition + heightOfBuilding > Height) {
+		cout << "Ups! Das Gebaeude ragt ueber die verfuegbare Flaeche hinaus." << endl;
 		return false;
 	}
 
+	// check if there are already other buildings in the area
 	if (FindOverlappingBuildings(xPosition, yPosition, widthOfBuilding, heightOfBuilding)) {
+		cout << "Kann kein Gebaeude setzen! Es existiert bereits eins an dieser Stelle!" << endl;
 		return false;
 	}
 
+	// setting new building is allowed
+	// set new building
 	for (int i = xPosition; i < xPosition + widthOfBuilding; i++) {
 		for (int j = yPosition; j < yPosition + heightOfBuilding; j++) {
 			plan[i][j] = type;
 		}
 	}
 
+	// return success
 	return true;
 }
 
+/// <summary>
+/// Action Delete Building.
+/// </summary>
+/// <returns></returns>
 bool DeleteBuilding() {
 	int heightOfBuilding;
 	int widthOfBuilding;
 	int xPosition;
 	int yPosition;
-	cout << "Hoehe: " << endl;
-	cin >> heightOfBuilding;
-	if (heightOfBuilding <= 0 || heightOfBuilding >= Height) {
-		return false;
-	}
+
+	// read and check width input
 	cout << "Breite: " << endl;
 	cin >> widthOfBuilding;
-	if (widthOfBuilding <= 0 || widthOfBuilding >= Width) {
+	if (widthOfBuilding < 0 || widthOfBuilding > Width) {
+		cout << "Ups! Breite ist zu klein oder zu groß." << endl;
 		return false;
 	}
+
+	// read and check height input
+	cout << "Hoehe: " << endl;
+	cin >> heightOfBuilding;
+	if (heightOfBuilding < 0 || heightOfBuilding > Height) {
+		cout << "Ups! Hoehe ist zu klein oder zu groß." << endl;
+		return false;
+	}
+
+	// read and check x coordinate input
 	cout << "x-Koordinate: " << endl;
 	cin >> xPosition;
-	if (xPosition <= 0 || xPosition + widthOfBuilding >= Width) {
+	if (xPosition < 0 || xPosition + widthOfBuilding > Width) {
+		cout << "Ups! Das Gebaeude ragt ueber die verfuegbare Flaeche hinaus." << endl;
 		return false;
 	}
+
+	// read and check y coordinate input
 	cout << "y-Koordinate: " << endl;
 	cin >> yPosition;
 	// test of size of:
@@ -137,7 +191,14 @@ bool DeleteBuilding() {
 	//cout << sizeof(plan[0]) << endl;
 	//cout << (sizeof(plan) / sizeof(plan[0])) << endl;
 	//cout << (sizeof(plan[0]) / sizeof(plan[0][0])) << endl;
-	if (yPosition <= 0 || yPosition + heightOfBuilding >= Height) {
+	if (yPosition < 0 || yPosition + heightOfBuilding > Height) {
+		cout << "Ups! Das Gebaeude ragt ueber die verfuegbare Flaeche hinaus." << endl;
+		return false;
+	}
+
+	// check if there are already other buildings in the area
+	if (FindOverlappingBuildings(xPosition, yPosition, widthOfBuilding, heightOfBuilding)) {
+		cout << "Kann kein Gebaeude setzen! Es existiert bereits eins an dieser Stelle!" << endl;
 		return false;
 	}
 
@@ -150,24 +211,34 @@ bool DeleteBuilding() {
 	return true;
 }
 
+/// <summary>
+/// Action Display Building. Prints current Building Plan.
+/// </summary>
+/// <returns></returns>
 bool DisplayBuilding() {
-	for (int j = 0; j < Height; j++) {
-		cout << "------- ";
-	}
 	for (int i = 0; i < Width; i++) {
+		cout << " -------";
+	}
+	cout << endl;
+
+	for (int i = 0; i < Height; i++) {
 		cout << "| ";
-		for (int j = 0; j < Height; j++) {
-			cout << buildings[plan[i][j]] + " | ";
+		for (int j = 0; j < Width; j++) {
+			cout << buildings[plan[j][i]] + " | ";
 		}
 		cout << endl;
-		for (int j = 0; j < Height; j++) {
-			cout << "------- ";
+		for (int j = 0; j < Width; j++) {
+			cout << " -------";
 		}
 		cout << endl;
 	}
 	return true;
 }
 
+/// <summary>
+/// Starts execution of user action.
+/// </summary>
+/// <param name="action">The given Action.</param>
 void HandleAction(Action action) {
 	if (action == Place) {
 		if (PlaceBuilding()) {
@@ -176,7 +247,7 @@ void HandleAction(Action action) {
 	}
 	else if (action == Delete) {
 		if (DeleteBuilding()) {
-			cout << "Bereich erfolgreich gelöscht!" << endl;
+			cout << "Bereich erfolgreich geloescht!" << endl;
 		}
 	}
 	else {
@@ -191,46 +262,41 @@ int main(int argc, char** argv)
 	for (int i = 0; i < argc; i++)
 		std::cout << argv[i] << std::endl;*/
 
-	if (argc != 3)
-		return INT32_MAX;
+	if (argc != 3) return INT32_MAX;
 
 	char* ptr;
-	Height = strtol(argv[1], &ptr, 10);
-	Width = strtol(argv[2], &ptr, 10);
+	Width = strtol(argv[1], &ptr, 10);
+	Height = strtol(argv[2], &ptr, 10);
 
-	cout << "Erstelleung eines Bauplans der Groesse: " << Height << "x" << Width << endl;
+	cout << "Erstelleung eines Bauplans der Groesse: " << Width << "x" << Height << endl;
 
+	// init 2 dimensional array plan 
+	// define first component (plan[i])
 	plan = new Building * [Width];
-
 	for (int i = 0; i < Width; i++) {
+		// foreach plan[i] define second component plan[i][second]
 		plan[i] = new Building[Height];
 		for (int j = 0; j < Height; j++) {
+			// init value is empty 
 			plan[i][j] = Empty;
 		}
 	}
 
+	// start user interaction
 	while (1) {
+
 		Action nextAction = ShowMenu();
+
 		if (nextAction == Exit) {
 			return 0;
 		}
 		else if (nextAction == Wrong) {
 			continue;
 		}
+		else {
+			HandleAction(nextAction);
+		}
 
-		HandleAction(nextAction);
 	}
 
 }
-
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
