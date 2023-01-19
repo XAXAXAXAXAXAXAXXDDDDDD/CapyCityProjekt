e5e148fffb4b8082b2715eed118edd83a013f230
XAXAXAXAXAXAXAXXDDDDDD <kaikong03@gmail.com>
2023-01-19 20:01:28 +0100

    Update

--- CapyCity Projekt/CapycitySim.h
+++ CapyCity Projekt/CapycitySim.h
@@ -16,7 +16,7 @@ private:
 public:
 	enum Action { Place, Delete, Displays, Exit, Wrong };
 
-	CapycitySim(int h, int w) : height(h), width(w), plan(new Building* [width]) {
+	CapycitySim(int w, int h) : height(h), width(w), plan(new Building* [width]) {
 		cout << "Erstelleung eines Bauplans der Groesse: " << width << "x" << height << endl;
 		for (int i = 0; i < width; i++) {
 			plan[i] = new Building[height];
